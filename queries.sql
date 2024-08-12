be3313e86874a600c03cc8b46ea64be780f3ab41
JuliaSh90 <144274072+JuliaSh90@users.noreply.github.com>
2024-08-12 22:38:10 +0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
+/*запрос, который считает общее количество покупателей из таблицы customers*/
 SELECT
     COUNT(customer_id) AS customers_count
 FROM customers;
-/запрос, который считает общее количество покупателей из таблицы customers/
