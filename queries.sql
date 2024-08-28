f7263067764bc5ca777d0cc9708413ff00c66a16
JuliaSh90 <144274072+JuliaSh90@users.noreply.github.com>
2024-08-28 22:15:44 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,19 +1,18 @@
 /*запрос, который считает общее количество покупателей из таблицы customers*/
-select
-    count(customer_id) as customers_count
+select count(customer_id) as customers_count
 from customers;
 
 /*запрос по определению десятки лучших продавцов*/
 select
-    concat(e.first_name, ' ', e.last_name) as seller,
-    count(s.sales_id) as operation,
-    floor(sum(p.price * s.quantity)) as income
-from sales as s
-inner join employees as e
-    on s.sales_person_id = e.employee_id
-inner join products as p
-    on s.product_id = p.product_id
-group by e.first_name, e.last_name
+    emp.first_name || ' ' || emp.last_name as seller,
+    count(sal.sales_id) as operations,
+    floor(sum(sal.quantity * pr.price)) as income
+from sales as sal
+left join products as pr
+    on sal.product_id = pr.product_id
+left join employees as emp
+    on sal.sales_person_id = emp.employee_id
+group by emp.first_name, emp.last_name
 order by income desc
 limit 10;
 
@@ -30,11 +29,9 @@ inner join products as p
 group by e.employee_id
 having
     avg(p.price * s.quantity) < (
-        select
-            avg(avg_income)
+        select avg(avg_income)
         from (
-            select
-                avg(s.quantity * p.price) as avg_income
+            select avg(s.quantity * p.price) as avg_income
             from employees as e
             inner join sales as s
                 on e.employee_id = s.sales_person_id
@@ -48,11 +45,11 @@ order by average_income;
 /*запрос, который содержит информацию о выручке по дням недели*/
 with tab as (
     select
+        p.price,
+        s.quantity,
         concat(e.first_name, ' ', e.last_name) as seller,
         to_char(s.sale_date, 'Day') as day_of_week,
-        to_char(s.sale_date, 'ID') as day_of_week_id,
-        p.price,
-        s.quantity
+        to_char(s.sale_date, 'ID') as day_of_week_id
     from sales as s
     inner join employees as e
         on s.sales_person_id = e.employee_id
@@ -85,7 +82,7 @@ order by age_category;
  и выручку, которую они принесли*/
 select
     to_char(s.sale_date, 'YYYY-MM') as selling_month,
-    count(distinct(s.customer_id)) as total_customers,
+    count(distinct s.customer_id) as total_customers,
     floor(sum(p.price * s.quantity)) as income
 from sales as s
 inner join products as p
